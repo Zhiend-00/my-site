fea81e84685973831b4da1288e9ea895c9f9461f
Zhiend-00 <sherbina_natashenka@mail.ru>
2026-05-03 12:36:46 +1000

    так надо

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 // src/api/index.js
 import axios from 'axios'
 
-const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:3000'
+const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:3000';
 
 const api = axios.create({
   baseURL: API_BASE
